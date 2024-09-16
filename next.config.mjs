526ac686764d3021c85206c12696ec139ec0a862
Somkene Ojukwu <somkeneoj@gmail.com>
2024-09-16 20:21:05 +0100

    tweets page updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [{
-            protocol: 'https',
-            hostname: 'images.unsplash.com'
-        }
-        ]
-    }
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'uploadthing.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'utfs.io'
+
+            }
+
+        ],
+    },
 };
 
 export default nextConfig;
